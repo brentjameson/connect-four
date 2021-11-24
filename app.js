b7afb641261b74ee2ed098db4096fe21fc4f4c1a
Brent Jameson <brentjameson@Brents-MBP.attlocal.net>
2021-11-24 09:18:44 -0500

    completed game with refactoring

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const gameContainer = document.querySelector(".game");
 const entryPointContainer = document.querySelector(".entryPoint");
 
 
-const gameBoardArray = ['0','1','2', '3', '4', '5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41']
+const gameBoardArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41]
 
 // below array from https://dev.to/fakorededamilola/building-a-connect-four-game-with-javascript-1f45
 const winningArray = [ 
@@ -65,213 +65,81 @@ function createDivsForGameBoard(array) {
   const entryPointDiv7 = document.querySelector('#entryPoint7')
   const entryPointArr = [entryPointDiv1, entryPointDiv2, entryPointDiv3, entryPointDiv4, entryPointDiv5, entryPointDiv6, entryPointDiv7]
 
-  const column1 = ['35', '28', '21', '14', '7', '0']
-  const column2 = ['36', '29', '22', '15', '8', '1']
-  const column3 = ['37', '30', '23', '16', '9', '2']
-  const column4 = ['38', '31', '24', '17', '10', '3']
-  const column5 = ['39', '32', '25', '18', '11', '4']
-  const column6 = ['40', '33', '26', '19', '12', '5']
-  const column7 = ['41', '34', '27', '20', '13', '6']
+  const column1 = [35, 28, 21, 14, 7, 0]
+  const column2 = [36, 29, 22, 15, 8, 1]
+  const column3 = [37, 30, 23, 16, 9, 2]
+  const column4 = [38, 31, 24, 17, 10, 3]
+  const column5 = [39, 32, 25, 18, 11, 4]
+  const column6 = [40, 33, 26, 19, 12, 5]
+  const column7 = [41, 34, 27, 20, 13, 6]
 
   const columns = [column1, column2, column3, column4, column5, column6, column7]
-
-  gameAndEntryContainer.addEventListener('click', function(e){
-
-  });
-
+  
+  function insertGamePieces(columnArr) {
+    for(let sq of columnArr) {
+      let sq1 = document.getElementsByClassName(sq)
+      if(sq1[0].hasChildNodes()){
+        continue;
+      }
+      else (!sq1[0].hasChildNodes());  
+        const newDiv = document.createElement("div")
+        if (counter % 2 === 0){
+          newDiv.classList.add('piecesRed')
+        sq1[0].appendChild(newDiv);
+        return; 
+        }
+        else {
+          newDiv.classList.add('piecesBlue')
+        sq1[0].appendChild(newDiv);
+        return; 
+        }
+    }
+  }
+  
   let counter = '0'
 
   gameAndEntryContainer.addEventListener('click', function(e){
     // console.log(e.target);
       if(e.target === entryPointDiv1) {
         counter++;
-        for(let sq of column1) {
-          let sq1 = document.getElementsByClassName(sq)
-          if(sq1[0].hasChildNodes()){
-            continue;
-          }
-          else (!sq1[0].hasChildNodes());  
-            const newDiv = document.createElement("div")
-            //next 3 lines = trying to link the number of the piece class to the number of the gameboard class
-            let squareOneOfZero = JSON.stringify(sq1[0].classList)
-            console.log(squareOneOfZero)
-            newDiv.classList.add(squareOneOfZero)
-            if (counter % 2 === 0){
-              newDiv.classList.add('piecesRed')
-            sq1[0].appendChild(newDiv);
-            return; 
-            }
-            else {
-              newDiv.classList.add('piecesBlue')
-            sq1[0].appendChild(newDiv);
-            return; 
-            }
-    
-      }
+        insertGamePieces(column1)
 
     }
       if(e.target === entryPointDiv2) {
         counter++;
-    console.log (counter);
-        for(let sq of column2) {
-          let sq1 = document.getElementsByClassName(sq)
-          if(sq1[0].hasChildNodes()){
-            console.log("it's working")
-            continue;
-  
-          }
-          else (!sq1[0].hasChildNodes());  
-            const newDiv = document.createElement("div")
-            if (counter % 2 === 0){
-              newDiv.classList.add('piecesRed')
-            sq1[0].appendChild(newDiv);
-            return; 
-            }
-            else {
-              newDiv.classList.add('piecesBlue')
-            sq1[0].appendChild(newDiv);
-            return; 
-            }
-    
-      }
+        insertGamePieces(column2)
 
     }
       if(e.target === entryPointDiv3) {
         counter++;
-    console.log (counter);
-        for(let sq of column3) {
-          let sq1 = document.getElementsByClassName(sq)
-          if(sq1[0].hasChildNodes()){
-            console.log("it's working")
-            continue;
-  
-          }
-          else (!sq1[0].hasChildNodes());  
-            const newDiv = document.createElement("div")
-            if (counter % 2 === 0){
-              newDiv.classList.add('piecesRed')
-            sq1[0].appendChild(newDiv);
-            return; 
-            }
-            else {
-              newDiv.classList.add('piecesBlue')
-            sq1[0].appendChild(newDiv);
-            return; 
-            }
-    
-      }
+        insertGamePieces(column3)
 
     }
       if(e.target === entryPointDiv4) {
         counter++;
-    console.log (counter);
-        for(let sq of column4) {
-          let sq1 = document.getElementsByClassName(sq)
-          if(sq1[0].hasChildNodes()){
-            console.log("it's working")
-            continue;
-  
-          }
-          else (!sq1[0].hasChildNodes());  
-            const newDiv = document.createElement("div")
-            if (counter % 2 === 0){
-              newDiv.classList.add('piecesRed')
-            sq1[0].appendChild(newDiv);
-            return; 
-            }
-            else {
-              newDiv.classList.add('piecesBlue')
-            sq1[0].appendChild(newDiv);
-            return; 
-            }
-    
-      }
+        insertGamePieces(column4)
 
     }
       if(e.target === entryPointDiv5) {
         counter++;
-    console.log (counter);
-        for(let sq of column5) {
-          let sq1 = document.getElementsByClassName(sq)
-          if(sq1[0].hasChildNodes()){
-            console.log("it's working")
-            continue;
-  
-          }
-          else (!sq1[0].hasChildNodes());  
-            const newDiv = document.createElement("div")
-            if (counter % 2 === 0){
-              newDiv.classList.add('piecesRed')
-            sq1[0].appendChild(newDiv);
-            return; 
-            }
-            else {
-              newDiv.classList.add('piecesBlue')
-            sq1[0].appendChild(newDiv);
-            return; 
-            }
-    
-      }
+        insertGamePieces(column5)
 
     }
       if(e.target === entryPointDiv6) {
         counter++;
-    console.log (counter);
-        for(let sq of column6) {
-          let sq1 = document.getElementsByClassName(sq)
-          if(sq1[0].hasChildNodes()){
-            console.log("it's working")
-            continue;
-  
-          }
-          else (!sq1[0].hasChildNodes());  
-            const newDiv = document.createElement("div")
-            if (counter % 2 === 0){
-              newDiv.classList.add('piecesRed')
-            sq1[0].appendChild(newDiv);
-            return; 
-            }
-            else {
-              newDiv.classList.add('piecesBlue')
-            sq1[0].appendChild(newDiv);
-            return; 
-            }
-    
-      }
+        insertGamePieces(column6)
 
     }
       if(e.target === entryPointDiv7) {
         counter++;
-    console.log (counter);
-        for(let sq of column7) {
-          let sq1 = document.getElementsByClassName(sq)
-          if(sq1[0].hasChildNodes()){
-            console.log("it's working")
-            continue;
-  
-          }
-          else (!sq1[0].hasChildNodes());  
-            const newDiv = document.createElement("div")
-            if (counter % 2 === 0){
-              newDiv.classList.add('piecesRed')
-            sq1[0].appendChild(newDiv);
-            return; 
-            }
-            else {
-              newDiv.classList.add('piecesBlue')
-            sq1[0].appendChild(newDiv);
-            return; 
-            }
-    
-      }
-
+        insertGamePieces(column7)
     }
 
   })
 
   // some elements of the below function are from https://dev.to/fakorededamilola/building-a-connect-four-game-with-javascript-1f45
-function checkWon() {
-  let squares = document.querySelectorAll('.game div')
+// function checkWon() {
+//   let squares = document.querySelectorAll('.game div')
 
 //   for (let square of winningArray) {
 //     I like this concept, but need to find a way to check to see if squares are red or blue and then match them up to the winnningArray. The below 2 lines are what I need to figure out
@@ -285,4 +153,32 @@ function checkWon() {
 //       setTimeout(() => restart.style.display = 'flex', 500)
 //     }
 //   }
+// }
+
+// 
+
+function checkWon() {
+  let squares = document.querySelectorAll('.piecesBlue')
+  for (let square of winningArray) {
+    console.log(square)
+  square.every(containsNum)
+  };
+  alert('Blue wins!')
 }
+
+function containsNum(val){
+  let pieces = document.querySelectorAll('.piecesBlue')
+  for (let square of winningArray) {
+    return val.classList.contains(Json.stringify(winningArray))
+  }
+}
+
+
+//     I like this concept, but need to find a way to check to see if squares are red or blue and then match them up to the winnningArray. The below 2 lines are what I need to figure out
+// To win: same color of pieces (find using classList) contain all 4 numbers in a winning array (use 'contains' or 'includes' with classList)
+    // if(
+
+
+      
+
+    
